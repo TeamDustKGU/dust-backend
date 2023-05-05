64356e67dbb1d414147e51af5fe9e1e695db16f6
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-05-05 09:51:11 +0900

    initial setting

---
+++ src/main/java/TeamDustKGU/dustbackend/DustBackendApplication.java
@@ -0,0 +1,13 @@
+package TeamDustKGU.dustbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DustBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DustBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TeamDustKGU/dustbackend/DustBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package TeamDustKGU.dustbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DustBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
