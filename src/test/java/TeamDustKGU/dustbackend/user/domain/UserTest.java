9829a8a83201cc9d9fc8c405fe1dfd2dddc54590
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-05-15 16:11:50 +0900

    refactor: User 초기화 방식 변경 및 getNicknameValue null 검증 로직 추가

--- src/test/java/TeamDustKGU/dustbackend/user/domain/UserTest.java
+++ src/test/java/TeamDustKGU/dustbackend/user/domain/UserTest.java
@@ -3,65 +3,56 @@ package TeamDustKGU.dustbackend.user.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static TeamDustKGU.dustbackend.fixture.UserFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("User 도메인 테스트")
 public class UserTest{
     @Test
-    @DisplayName("User를 생성한다.")
+    @DisplayName("User를 생성한다")
     public void createUser() {
-        Email email = Email.createEmail("qwer@gmail.com");
-        String password = "qwer1234";
-        Nickname nickname = Nickname.createNickname("dust");
-        User user = User.createUser(email, password, nickname);
+        User user = CHAERIN.toUser();
 
         assertAll(
                 () -> assertThat(user.getRole()).isEqualTo(Role.USER),
-                () -> assertThat(user.getEmailValue()).isEqualTo(email.getValue()),
+                () -> assertThat(user.getEmailValue()).isEqualTo(CHAERIN.getEmail()),
                 () -> assertThat(user.getAuth()).isEqualTo(1),
-                () -> assertThat(user.getPassword()).isEqualTo(password),
-                () -> assertThat(user.getNicknameValue()).isEqualTo(nickname.getValue()),
+                () -> assertThat(user.getPassword()).isEqualTo(CHAERIN.getPassword()),
+                () -> assertThat(user.getNickname()).isNull(),
                 () -> assertThat(user.getStatus()).isEqualTo(0)
         );
     }
 
     @Test
-    @DisplayName("User 닉네임을 변경한다.")
+    @DisplayName("User 닉네임을 변경한다")
     void updateNickname(){
-        //given
-        Email email = Email.createEmail("qwer@gmail.com");
-        String password = "qwer1234";
-        Nickname nickname = Nickname.createNickname("dust");
-        User user = User.createUser(email,password, nickname);
+        // given
+        User user1 = CHAERIN.toUser();
+        User user2 = SUNKYOUNG.toUser();
+        User user3 = SEOKHO.toUser();
 
-        //when
-        String change = "user1234";
-        user.updateNickname(change);
+        // when
+        user1.updateNickname("별명1");
+        user1.updateNickname("별명2");
+        user2.updateNickname("별명1");
 
-        //then
-        assertThat(user.getNicknameValue()).isEqualTo(change);
+        // then
+        assertThat(user1.getNicknameValue()).isEqualTo("별명2");
+        assertThat(user2.getNicknameValue()).isEqualTo("별명1");
+        assertThat(user3.getNickname()).isNull();
     }
 
     @Test
-    @DisplayName("이메일로 동일 사용자인지 확인한다.")
+    @DisplayName("이메일로 동일 사용자인지 확인한다")
     void isSameUser(){
-        //given
-        Email email = Email.createEmail("qwer@gmail.com");
-        String password = "qwer1234";
-        Nickname nickname = Nickname.createNickname("dust");
-        final User user = User.createUser(email,password, nickname);
-        User user1 = User.createUser(email,password, nickname);
-        User user2 = User.createUser(Email.createEmail("dust@gmail.com"), password, nickname);
+        // given
+        User user1 = CHAERIN.toUser();
+        User user2 = User.createUser(Email.from(CHAERIN.getEmail()), "password");
+        User user3 = User.createUser(Email.from("diff" + CHAERIN.getEmail()), "password");
 
-        //when
-        boolean result1 = user.isSameUser(user1);
-        boolean result2 = user.isSameUser(user2);
-
-        //then
-        assertAll(
-                () -> assertThat(result1).isTrue(),
-                () -> assertThat(result2).isFalse()
-        );
+        // then
+        assertThat(user1.isSameUser(user2)).isTrue();
+        assertThat(user1.isSameUser(user3)).isFalse();
     }
 }
