e4dbc6682c259e91f6f5dc09f39e92270e12ca64
SunKyoung Yoon <yskkjdh@gmail.com>
2023-05-17 16:19:13 +0900

    feat: Board 도메인 구현

---
+++ src/main/java/TeamDustKGU/dustbackend/board/Board.java
@@ -0,0 +1,62 @@
+package TeamDustKGU.dustbackend.board;
+
+import TeamDustKGU.dustbackend.comment.Comment;
+import TeamDustKGU.dustbackend.global.BaseTimeEntity;
+import TeamDustKGU.dustbackend.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name="board")
+public class Board extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name= "view", nullable = false)
+    private int view;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="writer_id")
+    private User writer;
+
+    //게시글 삭제시 달려있는 댓글 모두 삭제
+    @OneToMany(mappedBy = "board", cascade = ALL, orphanRemoval = true)
+    private List<Comment> commentList = new ArrayList<>();
+
+    @Builder
+    public Board(String title, String content, User writer){
+        this.title = title;
+        this.content = content;
+        this.view = 0;
+        this.writer = writer;
+    }
+
+    public static Board createBoard(String title, String content, User writer){
+        return new Board(title, content, writer);
+    }
+
+    public void updateTitle(String updateTitle){
+        this.title = updateTitle;
+    }
+
+    public void updateContent(String updateContent){
+        this.content = updateContent;
+    }
+}
