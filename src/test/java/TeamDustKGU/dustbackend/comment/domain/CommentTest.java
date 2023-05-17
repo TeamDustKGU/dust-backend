5db3e33cb48e02a96521001f1069937e8245c8d1
SunKyoung Yoon <yskkjdh@gmail.com>
2023-05-17 16:31:41 +0900

    test: CommentTest 추가

---
+++ src/test/java/TeamDustKGU/dustbackend/comment/domain/CommentTest.java
@@ -0,0 +1,47 @@
+package TeamDustKGU.dustbackend.comment.domain;
+
+import TeamDustKGU.dustbackend.board.Board;
+import TeamDustKGU.dustbackend.comment.Comment;
+import TeamDustKGU.dustbackend.user.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static TeamDustKGU.dustbackend.fixture.BoardFixture.BOARD_1;
+import static TeamDustKGU.dustbackend.fixture.CommentFixture.Comment_1;
+import static TeamDustKGU.dustbackend.fixture.UserFixture.CHAERIN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("Comment 도메인 테스트")
+public class CommentTest {
+    @Test
+    @DisplayName("댓글을 생성한다")
+    void createComment() {
+        User writer = CHAERIN.toUser();
+        Board board = BOARD_1.toBoard(writer);
+        Comment comment = Comment_1.toComment(writer,board);
+
+        assertAll(
+                () -> assertThat(comment.getComment()).isEqualTo(Comment_1.getComment()),
+                () -> assertThat(comment.getParent()).isEqualTo(Comment_1.getParent()),
+                () -> assertThat(comment.getWriter()).isEqualTo(writer),
+                () -> assertThat(comment.getBoard()).isEqualTo(board)
+        );
+    }
+
+    @Test
+    @DisplayName("자식 댓글을 생성한다")
+    void createChildComment() {
+        User writer = CHAERIN.toUser();
+        Board board = BOARD_1.toBoard(writer);
+        Comment parentComment = Comment_1.toComment(writer,board);
+        Comment childComment = new Comment("자식 댓글1",parentComment,writer,board);
+
+        assertAll(
+                () -> assertThat(childComment.getComment()).isEqualTo("자식 댓글1"),
+                () -> assertThat(childComment.getParent()).isEqualTo(parentComment),
+                () -> assertThat(childComment.getWriter()).isEqualTo(writer),
+                () -> assertThat(childComment.getBoard()).isEqualTo(board)
+        );
+    }
+}
