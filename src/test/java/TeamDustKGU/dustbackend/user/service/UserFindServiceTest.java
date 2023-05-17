74791f99c862e9d3f7ac5f3140453c90e3e33e2e
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2023-05-17 17:12:23 +0900

    feat: UserFindService 설계

---
+++ src/test/java/TeamDustKGU/dustbackend/user/service/UserFindServiceTest.java
@@ -0,0 +1,41 @@
+package TeamDustKGU.dustbackend.user.service;
+
+import TeamDustKGU.dustbackend.common.ServiceTest;
+import TeamDustKGU.dustbackend.global.exception.DustException;
+import TeamDustKGU.dustbackend.user.domain.User;
+import TeamDustKGU.dustbackend.user.exception.UserErrorCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static TeamDustKGU.dustbackend.fixture.UserFixture.SUNKYOUNG;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("User [Service Layer] -> UserFindService 테스트")
+public class UserFindServiceTest extends ServiceTest {
+    @Autowired
+    private UserFindService userFindService;
+
+    private User user;
+
+    @BeforeEach
+    void setup() {
+        user = userRepository.save(SUNKYOUNG.toUser());
+    }
+
+    @Test
+    @DisplayName("ID(PK)로 회원을 조회한다")
+    void findById() {
+        // when
+        User findUser = userFindService.findById(user.getId());
+
+        // then
+        assertThatThrownBy(() -> userFindService.findById(user.getId() + 100L))
+                .isInstanceOf(DustException.class)
+                .hasMessage(UserErrorCode.USER_NOT_FOUND.getMessage());
+
+        assertThat(findUser).isEqualTo(user);
+    }
+}
