8a8951a10c87853cf91f4912ae3e7e4f98e89c9b
SunKyoung Yoon <yskkjdh@gmail.com>
2023-05-24 03:52:06 +0900

    test: BoardService 관련 테스트 작성

---
+++ src/test/java/TeamDustKGU/dustbackend/board/service/BoardFindServiceTest.java
@@ -0,0 +1,45 @@
+package TeamDustKGU.dustbackend.board.service;
+
+import TeamDustKGU.dustbackend.board.domain.Board;
+import TeamDustKGU.dustbackend.common.ServiceTest;
+import TeamDustKGU.dustbackend.global.exception.DustException;
+import TeamDustKGU.dustbackend.user.domain.User;
+import TeamDustKGU.dustbackend.board.exception.BoardErrorCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static TeamDustKGU.dustbackend.fixture.BoardFixture.BOARD_1;
+import static TeamDustKGU.dustbackend.fixture.UserFixture.SUNKYOUNG;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("Board [Service Layer] -> BoardFindService 테스트")
+public class BoardFindServiceTest extends ServiceTest {
+    @Autowired
+    private BoardFindService boardFindService;
+
+    private User writer;
+    private Board board;
+
+    @BeforeEach
+    void setup() {
+        writer = userRepository.save(SUNKYOUNG.toUser());
+        board = boardRepository.save(BOARD_1.toBoard(writer));
+    }
+
+    @Test
+    @DisplayName("ID(PK)로 게시글을 조회한다")
+    void findById() {
+        // when
+        Board findBoard = boardFindService.findById(board.getId());
+
+        // then
+        assertThatThrownBy(() -> boardFindService.findById(board.getId() + 100L))
+                .isInstanceOf(DustException.class)
+                .hasMessage(BoardErrorCode.BOARD_NOT_FOUND.getMessage());
+
+        assertThat(findBoard).isEqualTo(board);
+    }
+}
---
+++ src/test/java/TeamDustKGU/dustbackend/board/service/BoardServiceTest.java
@@ -0,0 +1,115 @@
+package TeamDustKGU.dustbackend.board.service;
+
+import TeamDustKGU.dustbackend.board.domain.Board;
+import TeamDustKGU.dustbackend.board.exception.BoardErrorCode;
+import TeamDustKGU.dustbackend.common.ServiceTest;
+import TeamDustKGU.dustbackend.global.exception.DustException;
+import TeamDustKGU.dustbackend.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static TeamDustKGU.dustbackend.fixture.BoardFixture.BOARD_1;
+import static TeamDustKGU.dustbackend.fixture.UserFixture.CHAERIN;
+import static TeamDustKGU.dustbackend.fixture.UserFixture.SUNKYOUNG;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DisplayName("Board [Service Layer] -> BoardService 테스트")
+public class BoardServiceTest extends ServiceTest {
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private BoardFindService boardFindService;
+
+    private User writer1;
+    private User writer2;
+    private Board board;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    @BeforeEach
+    void setup() {
+        writer1 = userRepository.save(SUNKYOUNG.toUser());
+        writer2 = userRepository.save(CHAERIN.toUser());
+        board = boardRepository.save(BOARD_1.toBoard(writer1));
+    }
+
+    @Test
+    @DisplayName("게시글 등록에 성공한다")
+    void success() {
+        // when
+        Long boardId = boardService.create(writer1.getId(), "제목", "내용");
+
+        // then
+        Board findBoard = boardRepository.findById(boardId).orElseThrow();
+        assertAll(
+                () -> assertThat(findBoard.getWriter().getId()).isEqualTo(writer1.getId()),
+                () -> assertThat(findBoard.getTitle()).isEqualTo("제목"),
+                () -> assertThat(findBoard.getContent()).isEqualTo("내용"),
+                () -> assertThat(findBoard.getCreatedDate().format(formatter)).isEqualTo(LocalDateTime.now().format(formatter)),
+                () -> assertThat(findBoard.getModifiedDate().format(formatter)).isEqualTo(LocalDateTime.now().format(formatter))
+        );
+    }
+
+    @Nested
+    @DisplayName("게시글 수정")
+    class update {
+        @Test
+        @DisplayName("다른 사람의 게시글은 수정할 수 없다")
+        void throwExceptionByUpdateBoardOnlyWriter() {
+            // when - then
+            assertThatThrownBy(() -> boardService.update(writer2.getId(),board.getId(), "제목2", "내용2"))
+                    .isInstanceOf(DustException.class)
+                    .hasMessage(BoardErrorCode.UPDATE_BOARD_ONLY_WRITER.getMessage());
+        }
+
+        @Test
+        @DisplayName("게시글 수정에 성공한다")
+        void success() {
+            //given
+            boardService.update(writer1.getId(), board.getId(), "제목2","내용2");
+
+            // when
+            Board findBoard = boardFindService.findById(board.getId());
+
+            // then
+            assertAll(
+                    () -> assertThat(findBoard.getTitle()).isEqualTo("제목2"),
+                    () -> assertThat(findBoard.getContent()).isEqualTo("내용2"),
+                    () -> assertThat(findBoard.getModifiedDate().format(formatter)).isEqualTo(LocalDateTime.now().format(formatter))
+            );
+        }
+    }
+
+    @Nested
+    @DisplayName("게시글 삭제")
+    class delete {
+        @Test
+        @DisplayName("다른 사람의 게시글은 삭제할 수 없다")
+        void throwExceptionByDeleteBoardOnlyWriter() {
+            // when - then
+            assertThatThrownBy(() -> boardService.delete(writer2.getId(),board.getId()))
+                    .isInstanceOf(DustException.class)
+                    .hasMessage(BoardErrorCode.DELETE_BOARD_ONLY_WRITER.getMessage());
+        }
+
+        @Test
+        @DisplayName("게시글 삭제에 성공한다")
+        void success(){
+            // given
+            boardService.delete(writer1.getId(), board.getId());
+
+            // when - then
+            assertThatThrownBy(() -> boardFindService.findById(board.getId() + 100L))
+                    .isInstanceOf(DustException.class)
+                    .hasMessage(BoardErrorCode.BOARD_NOT_FOUND.getMessage());
+        }
+    }
+}
