97410b261b4b5dade10d17fc2fa9a29c5d0c5a22
SunKyoung Yoon <yskkjdh@gmail.com>
2023-05-24 03:47:03 +0900

    feat: BoardRequest(dto) 작성

---
+++ src/main/java/TeamDustKGU/dustbackend/board/controller/dto/BoardRequest.java
@@ -0,0 +1,14 @@
+package TeamDustKGU.dustbackend.board.controller.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public record BoardRequest(
+        @NotBlank(message = "제목 작성은 필수입니다.")
+        @Size(max = 20, message = "제목은 20자 이내로 작성해주세요.")
+        String title,
+
+        @NotBlank(message = "내용 작성은 필수입니다.")
+        String content
+) {
+}
