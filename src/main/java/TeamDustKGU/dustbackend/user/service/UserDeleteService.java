f54fdb1e5f7cf4501577bf745bfc42432e4f3d59
hongseokhodev <126336224+hongseokhodev@users.noreply.github.com>
2023-06-18 07:20:42 +0900

    refactor: UserDeleteService deleteById로 수정 refactor: 하단 공백 삭제

--- src/main/java/TeamDustKGU/dustbackend/user/service/UserDeleteService.java
+++ src/main/java/TeamDustKGU/dustbackend/user/service/UserDeleteService.java
@@ -1,6 +1,5 @@
 package TeamDustKGU.dustbackend.user.service;
 
-import TeamDustKGU.dustbackend.user.domain.User;
 import TeamDustKGU.dustbackend.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,12 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserDeleteService {
-    private final UserFindService userFindService;
     private final UserRepository userRepository;
 
     @Transactional
     public void deleteUser(Long userId) {
-        User user = userFindService.findById(userId);
-        userRepository.delete(user);
+        userRepository.deleteById(userId);
     }
 }
